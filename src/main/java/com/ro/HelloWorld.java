8bb56ca3c78a94f338a5d20c46565ca7312c9302
Roro <rorochen0903@gmail.com>
2021-03-09 16:31:38 +0800

    Student

--- src/main/java/com/ro/HelloWorld.java
+++ src/main/java/com/ro/HelloWorld.java
@@ -2,6 +2,17 @@ package com.ro;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
+        //Student st = new Student("1101","Eric", 60,80);\
+        //st.print();
+        Student[] sts = new Student[]{
+            new Student("1101","Eric", 60,80),
+                new Student("1102","Tomy", 60,80),
+                new Student("1103","Roro", 60,80),
+                new Student("1104","Alice", 60,80)
+        };
+        for (int i = 0; i < sts.length ; i++) {
+            sts[i].print();
+        }
     }
 }
