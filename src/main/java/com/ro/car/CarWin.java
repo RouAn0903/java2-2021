df01bd3cf5d56c2235afa4d39f3f2de3a9c0bd71
Roro <rorochen0903@gmail.com>
2021-06-01 15:01:20 +0800

    carwin

---
+++ src/main/java/com/ro/car/CarWin.java
@@ -0,0 +1,14 @@
+package com.ro.car;
+
+import javax.swing.*;
+
+public class CarWin {
+    static int width=600;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(width, 40);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
